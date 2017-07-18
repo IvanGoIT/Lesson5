3cb3e9e512b5e8984defc34767f3342732030885
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-07-18 19:22:45 +0300

    Инициализация проекта

---
+++ src/ObjectOrientedProgramming.java
@@ -0,0 +1,12 @@
+public class ObjectOrientedProgramming {
+
+    final int xx;
+
+    public ObjectOrientedProgramming() {
+        this.xx = 10;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[0];
+    }
+}
